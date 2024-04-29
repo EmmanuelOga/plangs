01f98c6ac8a416a60e2e4bdb91ea0d3e772f7646
Emmanuel Oga <contact@emmanueloga.com>
2024-04-29 15:44:01 -0700

    move pixi stuff from .env to pixi.sh

--- pixi.sh
+++ pixi.sh
@@ -1,8 +1,28 @@
-# "Activate" the environment: create a new shell with pixi settings and project environment variables loaded.
-bash --init-file <(cat <<- EOF
-set -a .                           # Export all variables set by the following steps.
-source ~/.bashrc                   # Load default bash settings.
-eval "\$(pixi shell-hook -s bash)" # Load pixi shell settings.
-source .env                        # Load project environment variables.
+#!/bin/bash
+
+# Define the bash configuration into a variable.
+## read details:
+### -r: raw mode, it doesn't allow backslashes to escape any characters.
+### -d '': sets delimiter to null, so it reads the whole input.
+read -r -d '' bash_config <<-'EOF'
+# Load default bash settings.
+source ~/.bashrc
+
+# Use pixi beta feature: allow loading pypi packages.
+export PIXI_BETA_WARNING_OFF=true
+
+# Load pixi shell settings.
+eval "$(pixi shell-hook -s bash)"
+
+# Use folder-local installed go.
+export GOPATH=$(pwd)/.go
+export PATH=$PATH:$(pwd)/.go/bin
+
+# Load project environment variables.
+## Export variables defined in .env file.
+set -a 
+source .env
 EOF
-)
+
+# "<()" is a process substitution, it allows to run a command and use its output as a file.
+bash --init-file <(echo "$bash_config")
